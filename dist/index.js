ac157c96e692258a9f408a7fb77d88bd4469f6c1
lalithkumart <lalithkumart@flavorcloud.com>
2022-01-10 11:35:20 +0530

    created new module for Express framework

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.FlclLoggerLb4 = exports.FlclError = exports.FlclLogger = exports.FlclMsgController = void 0;
+exports.FlclLoggerExpress = exports.FlclLoggerLb4 = exports.FlclError = exports.FlclLogger = exports.FlclMsgController = void 0;
 const flclLogger_1 = require("./src/flclLogger");
 Object.defineProperty(exports, "FlclLogger", { enumerable: true, get: function () { return flclLogger_1.FlclLogger; } });
 const flclError_1 = require("./src/flclError");
@@ -9,4 +9,6 @@ const flclMsgHandler_1 = require("./src/flclMsgHandler");
 Object.defineProperty(exports, "FlclMsgController", { enumerable: true, get: function () { return flclMsgHandler_1.FlclMsgController; } });
 const flclLoggerLb4_1 = require("./src/flclLoggerLb4");
 Object.defineProperty(exports, "FlclLoggerLb4", { enumerable: true, get: function () { return flclLoggerLb4_1.FlclLoggerLb4; } });
+const flclLoggerExpress_1 = require("./src/flclLoggerExpress");
+Object.defineProperty(exports, "FlclLoggerExpress", { enumerable: true, get: function () { return flclLoggerExpress_1.FlclLoggerExpress; } });
 //# sourceMappingURL=index.js.map
